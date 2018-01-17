01792e287a372ffdbc74b2b9ed98ed360fcef248
Amy Churchwell and Luke Bertram <luke@>
2018-01-17 09:47:22 -0800

    Add keg constructor and display in bootstrap cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Taproom';
+  kegs: Keg[] = [
+    new Keg('Butterbeer', 200, 20, `HufflePuff`),
+    new Keg('Dragon\'s Breath', 500, 5, 'Spielburg Cellars'),
+    new Keg('Fireball', 10, 2, 'Volcano vineyards on a volcano'),
+    new Keg('Motor Oil', 0, 6, 'Mobil')
+  ];
+}
+
+export class Keg {
+  public pintsRemaining: number = 124;
+  constructor(
+    public name: string,
+    public alcoholContent: number,
+    public pintPrice: number,
+    public brand: string,
+  ){ }
 }
